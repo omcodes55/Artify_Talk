52dbd75c0faf91bd90cae4005d8a0974494754ae
omcodes55 <vadherom999@gmail.com>
2026-01-02 22:45:08 +0530

    Updated

--- client/artify_talk/src/App.jsx
+++ client/artify_talk/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen '>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/result" element={<Result />} />
+      <Route path="/buy-credit" element={<BuyCredit />} />
+    </Routes>
+
     </div>
   )
 }
--- client/artify_talk/vite.config.js
+++ client/artify_talk/vite.config.js
@@ -1,12 +1,10 @@
-import { reactRouter } from "@react-router/dev/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    reactRouter(),
-    tsconfigPaths(),
+    react(),
   ],
 });
